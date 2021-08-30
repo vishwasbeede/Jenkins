193baec593d9e3c3895be3c84172d58b0bb266ed
Vishwas B G <vishwasbeede@gmail.com>
2021-08-30 12:38:22 +0530

    feature: added display time

--- python_jenkins_trial.py
+++ python_jenkins_trial.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-
+import random
+import datetime 
 print("Hello world")
+print("jenkins testing the data")
+
+
+list1=[1,2,4,5,6,7,8]
+print(random.choice(list1))
+print("Current time is as below")
+print("\n"+str(datetime.datetime.now()))
